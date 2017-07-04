7f090d4a8253c7f69888fa0e4400841a483707af
Nathaniel Eliot <temujin9@t9productions.com>
2017-07-03 21:57:50 -0500

    Adding fail to meh, some further cleanup

--- meh.py
+++ meh.py
@@ -2,6 +2,8 @@ import click
 
 ## Meh-driven development is the wave of the future. I defy you to prove me wrong.
 def meh(*args, **kwargs):
-  click.echo("MEH:\t"+repr(args)+" "+repr(kwargs))
+  click.echo("MEH:\t"+str(args)+", "+str(kwargs))
 
-
+def fail(string,code=1):
+  click.echo("FAIL("+str(code)+"): "+str(string))
+  exit(code)
