bb0dba5b47c32fe39f2cf2a1d226ff1c7cfd08e3
Nathaniel Eliot <temujin9@t9productions.com>
2017-07-03 21:28:53 -0500

    Simplifying interface to s3 org

--- s3/org.py
+++ s3/org.py
@@ -8,11 +8,11 @@ from lightcycle.pytf import dsl
 from lightcycle.meh import meh
 
 class Organization():
-  def __init__(self, name='', bucket='', path='', table='', region='us-east-1'):
+  def __init__(self, name='', path='', region='us-east-1'):
+    root, _, subpath = path.partition("/")
     self.name = name
-    self.bucket = bucket
+    self.root = root
     self.path = path
-    self.table = table
     self.region = region
     return
 
@@ -30,23 +30,23 @@ class Organization():
     '''
     # Is the requested DynamoDB path occupied?
     try:
-      lockID = self.bucket+"/"+self.path+"/org.tfstate-md5"
-      if "Item" in boto3.client("dynamodb").get_item(TableName=self.table,Key={"LockID":{"S":lockID}}):
-        raise Exception("dynamdob://"+self.table+"/"+lockID+" is already in use.")
+      lockID = self.root+"/"+self.path+"/org.tfstate-md5"
+      if "Item" in boto3.client("dynamodb").get_item(TableName=self.root,Key={"LockID":{"S":lockID}}):
+        raise Exception("dynamdob://"+self.root+"/"+lockID+" is already in use.")
     except botocore.exceptions.ClientError as err:
       if err.response["Error"]["Code"] != "ResourceNotFoundException":
-        raise Exception("Tried using "+self.table+" but received: "+str(err))
+        raise Exception("Tried using "+self.root+" but received: "+str(err))
     # Is the requested S3 path occupied?
     try:
-      if "Contents" in boto3.client("s3").list_objects_v2(Bucket=self.bucket, Prefix=self.path):
-        raise Exception("s3://"+self.bucket+"/"+self.path+" is already in use.")
+      if "Contents" in boto3.client("s3").list_objects_v2(Bucket=self.root, Prefix=self.path):
+        raise Exception("s3://"+self.root+"/"+self.path+" is already in use.")
     except botocore.exceptions.ClientError as err:
       if err.response["Error"]["Code"] != "NoSuchBucket":
-        raise Exception("Tried using "+self.bucket+" but received: "+str(err))
+        raise Exception("Tried using "+self.root+" but received: "+str(err))
 
   def bucket_exists(self):
     try:
-      boto3.resource("s3").Bucket(self.bucket).load()
+      boto3.resource("s3").Bucket(self.root).load()
       return True
     except botocore.exceptions.ClientError as err:
       if err.response["Error"]["Message"] == "NotFound":
@@ -55,7 +55,7 @@ class Organization():
 
   def table_exists(self):
     try:
-      boto3.resource("dynamodb").Table(self.table).load()
+      boto3.resource("dynamodb").Table(self.root).load()
       return True
     except botocore.exceptions.ClientError as err:
       if err.response["Error"]["Code"] == "ResourceNotFoundException":
@@ -67,13 +67,13 @@ class Organization():
     # template out org terraform
     tf = dsl.TerraformDsl()
     tf.provider("aws", region = self.region)
-    if not self.bucket_exists():
+    if not self.root_exists():
       tf.resource("aws_s3_bucket","org",
-        bucket = self.bucket,
+        bucket = self.root,
       )
-    if not self.table_exists():
+    if not self.root_exists():
       tf.resource("aws_dynamodb_table","org",
-        name = self.table,
+        name = self.root,
         hash_key = "LockID",
         attribute = [{
           "name": "LockID",
@@ -99,9 +99,9 @@ class Organization():
     tf.terraform(
       backend = {
         "s3": {
-          "bucket":         self.bucket,
+          "bucket":         self.root,
           "key":            self.path + "/org.tfstate",
-          "dynamodb_table": self.table,
+          "dynamodb_table": self.root,
           "region":         self.region,
         }
       }
@@ -109,7 +109,7 @@ class Organization():
     tf.output("org", value = self.name)
     tf.output("path", value = self.path)
     tf.output("region", value = self.region)
-    tf.output("table", value = self.table)
+    tf.output("table", value = self.root)
     # rerun terraform
     orgf = open(tmpd + "/org.tf","w")
     orgf.write(tf.json())
